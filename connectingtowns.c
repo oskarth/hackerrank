660b0b36de349b2da8721704c54d128f264c91f2
oskarth <me@oskarth.com>
2015-02-08 20:40:23 +0100

    moar

--- connectingtowns.c
+++ connectingtowns.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main () {
-  return 0;
+  int i, j, t, n;
+  scanf("%d", &t);
+
+  for (i = 0; i < t; i++) {
+    scanf("%d", &n); // number of towns
+    int routes[n-1];
+    int paths = 1;
+
+    for (j = 0; j < (n-1); j++) {
+      scanf("%d ", &routes[j]); // number of routes from j to j+1
+      paths = (paths * routes[j]) % 1234567;
+    }
+
+    printf("%d\n", paths);
+  }
 }
---
+++ eventree.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Skiena connectivity p 487 (pdf style)
+
+int main() {
+  int i, j, m, n;
+  scanf("%d %d", &n, &m); // #vertices and #edges
+
+  for (i = 0; i < m; i++) {
+    // scan each line for integers ui vi
+  }
