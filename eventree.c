7ad7522f921906b08fddb4c28e98866d7e5076d1
oskarth <me@oskarth.com>
2015-03-30 23:12:58 +0200

    eventree dfs

--- eventree.c
+++ eventree.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
 
-// Skiena connectivity p 487 (pdf style)
+#define MAXN 100
+
+int tree[MAXN][MAXN];
+int visited[MAXN];
+int count;
+
+int dfs(int n, int v) {
+  int i, j, sub, result;
+
+  visited[n] = 1;
+  result = 0;
+
+  for (i=0; i<v; i++) {
+    if (tree[n][i] == 1 && visited[i] == 0) {
+      sub = dfs(i,v);
+      if (sub % 2 == 0)
+        count++;
+      result += sub;
+    }
+  }
+  result++;
+  return result;
+}
 
 int main() {
-  int i, j, m, n;
-  scanf("%d %d", &n, &m); // #vertices and #edges
+  int i, e, v, v1, v2;
 
-  for (i = 0; i < m; i++) {
-    // scan each line for integers ui vi
+  scanf("%d %d", &v, &e);
+
+  for (i=0; i<e; i++) {
+    scanf("%d %d", &v1, &v2);
+    tree[v1-1][v2-1] = 1;
+    tree[v2-1][v1-1] = 1;
   }
+
+  dfs(0, v);
+  printf("%d\n", count);
+
+  return 0;
+}
